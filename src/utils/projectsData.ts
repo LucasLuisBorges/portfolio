97d7838aa0ba5219ace6d6364dfbb04bcf194bb4
LucasLuisBorges <LucasLuisBorges1205@gmail.com>
2022-08-15 20:02:48 -0300

    update date

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -3,42 +3,49 @@ export const projects = [
     name: 'Go finance',
     desc: 'This finances to.do',
     link: 'GoFinances',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'RentX',
     desc: 'This car aplication',
     link: 'App-RentX',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'Twitter',
     desc: 'This twitter web site clone',
     link: 'Twitter-Clone',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'ignite feed',
     desc: 'This user feedback aplication',
     link: 'ignite-feed',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'ignite timer',
     desc: 'A timer with history',
     link: 'ignite-timer',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'EasyChurras',
     desc: 'An app for grillers',
     link: 'EasyChurras',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
   },
   {
     name: 'FlappyBird',
     desc: 'This flappybird game in web',
     link: 'FlappyBird',
-    image: 'https://github.com/LucasLuisBorges.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png',
   },
 ];
